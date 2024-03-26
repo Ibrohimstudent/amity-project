a36d34cca68eb750291645cedfa010ee9ec962b8
Ibrohimstudent <itprogrammer75@gmail.com>
2024-03-26 15:15:40 +0500

    code

--- src/components/Pages/category.jsx
+++ src/components/Pages/category.jsx
@@ -16,43 +16,7 @@ function Category(props) {
 
     }
 
-      const columns = [
-        {
-            title: 'ID',
 
-
-            dataIndex: 'idx',
-        },
-        {
-            title: 'Name',
-
-            dataIndex: 'name',
-        },
-
-        {
-            title: 'Actions ',
-            render: (item)=> <div>
-                <i className={"bi bi-pencil  mx-2 text-primary "} onClick={() => editItem(item)}></i>
-
-
-                <Popconfirm
-                    title="Delete the task"
-                    description="Are you sure to delete this task?"
-                    onConfirm={() => confirm(item)}
-                    onCancel={cancel}
-                    okText="Yes"
-                    cancelText="No"
-                >
-                    <i className={"bi bi-trash3  mx-2 text-danger "}></i>
-
-                </Popconfirm>
-
-
-            </div>
-        }
-
-
-    ];
     const confirm = (e) => {
         console.log(e);
         message.success('Deleted');
@@ -66,27 +30,78 @@ function Category(props) {
 
 
     return (
-        <div style={{width:"95%"}} className={'container  position-relative my-4'}>
+        <div style={{width: "95%"}} className={'container  position-relative my-4'}>
+            <Input.Search style={{width: "300px"}} placeholder={"Search..."}
+                          onSearch={(value) => {
+                              setSearch(value)
+                          }}
+                          onChange={(e) => {
+                              setSearch(e.target.value)
+                          }}
 
-           
+            />
 
 
-            <Drawer  isModalOpen={open} setIsModalOpen={setOpen}/>
+            <Drawer isModalOpen={open} setIsModalOpen={setOpen}/>
 
 
             <Button type={"primary"} onClick={() => setOpen(prevState => !prevState)}
-                     className={"float-end "}>Add
+                    className={"float-end "}>Add
             </Button>
 
 
             <Divider/>
-            <h2 style={{fontWeight:"400"}}>Category List</h2>
-
-            <Table
 
 
-                columns={columns}
-                dataSource={category}
+            <h2 style={{fontWeight: "400"}}>Category List</h2>
+
+            <Table columns={[
+                {
+                    title: 'ID',
+                    dataIndex: 'idx',
+                    filteredValue: [search],
+                    onFilter: (value, record) => {
+                        return (
+                            String(record.name)
+                                .toLowerCase()
+                                .includes(value.toLowerCase()) ||
+                            String(record.idx)
+                                .toLowerCase()
+                                .includes(value.toLowerCase())
+                        );
+                    }
+                },
+                {
+                    title: 'Name',
+
+                    dataIndex: 'name',
+                },
+
+                {
+                    title: 'Actions ',
+                    render: (item) => <div>
+                        <i className={"bi bi-pencil  mx-2 text-primary "} onClick={() => editItem(item)}></i>
+
+
+                        <Popconfirm
+                            title="Delete the task"
+                            description="Are you sure to delete this task?"
+                            onConfirm={() => confirm(item)}
+                            onCancel={cancel}
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <i className={"bi bi-trash3  mx-2 text-danger "}></i>
+
+                        </Popconfirm>
+
+
+                    </div>
+                },
+
+
+            ]}
+                   dataSource={category}
             >
 
 
