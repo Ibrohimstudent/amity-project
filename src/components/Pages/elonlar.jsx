f6188bef9d6c91ef09793d800e7edd9525b86485
Ibrohimstudent <itprogrammer75@gmail.com>
2024-03-26 16:16:17 +0500

    code

--- src/components/Pages/elonlar.jsx
+++ src/components/Pages/elonlar.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Drawer from "./drawerElonlar";
+import {Button, Divider} from "antd";
 
 function Elonlar(props) {
+    const [open, setOpen] = useState(false);
     return (
-        <div>Elonlar</div>
+        <div style={{width:"95%"}} className={'container position-relative my-4'}>
+            <h2 style={{fontSize: "30px", fontWeight: "400", position: "absolute"}}>E'lonlar</h2>
+            <Drawer open={open} setOpen={setOpen}/>
+
+
+            <button style={{
+                backgroundColor: "#FEC200", width: "150px", fontWeight: "400",color:"white", fontSize: "16px", borderRadius: "5px",
+                border: "1px solid transparent", marginBottom: "20px"
+            }} onClick={() => setOpen(prevState => !prevState)}
+                    className={"float-end "}>+ E'lon yozish
+            </button>
+
+
+            <Divider/>
+        </div>
     );
 }
 
